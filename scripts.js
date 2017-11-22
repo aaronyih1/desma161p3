39d58b111997346ff830d4c6127fc9e5fd1fd22f
Aaron Yih <aaronyih@Aarons-MacBook-Pro.local>
2017-11-22 14:24:17 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,61 @@
+      var pubKey = config.PUB_KEY;
+      var subKey = config.SUB_KEY;
+      // Create PubNub Socket Handler
+      const pubnub = new PubNub({
+          publishKey   : 'empty'
+      ,   ssl          : true
+      ,   subscribeKey : 'sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe'
+      });
+      var session_Id = 0;
+
+      var pubnubPersonal = new PubNub({
+        publishKey: pubKey,
+        subscribeKey: subKey,
+      })
+
+      // Subscribe to Twitter feed
+      console.log("Subscribing to Live Twitter Stream.");
+      pubnub.subscribe({ channels: ['pubnub-twitter'] });
+      pubnubPersonal.subscribe({
+        channels: ['sentiment-analysis']
+      })
+
+      // Add Socket Event Function Handlers
+      pubnub.addListener({
+
+        message: function(m) {
+          //console.log(m);
+          var msg = m.message;
+
+          // We are filtering tweets based on Trump, trump, or any capitilization of POTUS
+          if (msg.text.match(/([T|t]rump|[P|p][O|o][T|t][U|u][S|s])/g)) {
+            session_Id++;
+            // Where we publish tweets for sentiment analysis
+            var publishConfig = {
+              channel : "sentiment-analysis",
+              message : {"session_id":session_Id,"text":msg.text, "sentiment":msg.session_sentiment }
+            }
+            //console.log(msg.text);
+            pubnubPersonal.publish(publishConfig, function(status, response) {
+              if (status.error) {
+                console.log(status, response);
+              }
+            })
+          }
+        }
+
+      })
+      pubnubPersonal.addListener({
+        message: function(m) {
+          //console.log(m);
+          var msg = m.message;
+          //console.log(m);
+          processData(msg);
+        }
+      //     status  : statusEvent => console.log(statusEvent)
+      // ,   message : message     => console.log(message)
+      });
+      function processData(data) {
+        //console.log(data);
+        document.querySelector('#feed').innerHTML = data.text;
+      }
